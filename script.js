b3d6db8e807a1c7d2b0f0221d0e7b37c1fa4faf4
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-30 15:01:39 -0400

    add code for handling switching report duration

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const uri = 'https://nursh.github.io/FM-Time-Tracking-Dashboard/data.json';
 
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
+
+const reportDetails = document.querySelector('.report-details');
+const reportDuration = document.querySelector('.report-duration');
+const durationLinks = reportDuration.querySelectorAll('a');
+const activeDuration = reportDuration.querySelector('.active');
+
+let reportData = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(data => reportData = data);
+
+  durationLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      const currentActiveLink = reportDuration.querySelector('.active');
+      currentActiveLink.setAttribute('class', '');
+      this.setAttribute('class', 'active');
+    })
+  })
+});
+
+function displayDetails() {
+  
+}
