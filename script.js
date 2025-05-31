92506f42837b5e2c9adef0b2e531a9842d5d47a1
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-31 16:16:29 -0400

    handle tablet size

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
-const uri = 'https://nursh.github.io/FM-Time-Tracking-Dashboard/data.json';
+const uri = "https://nursh.github.io/FM-Time-Tracking-Dashboard/data.json";
 
-
-const reportDetails = document.querySelector('.report-details');
-const reportDuration = document.querySelector('.report-duration');
-const durationLinks = reportDuration.querySelectorAll('a');
-const activeDuration = reportDuration.querySelector('.active');
+const reportDetails = document.querySelector(".report-details");
+const reportDuration = document.querySelector(".report-duration");
+const durationLinks = reportDuration.querySelectorAll("a");
+let currentActiveLink = reportDuration.querySelector(".active");
 
 let reportData = [];
 
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('./data.json')
-    .then(response => response.json())
-    .then(data => reportData = data);
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      reportData = data;
+      main(currentActiveLink.textContent.toLowerCase());
+    });
 
-  durationLinks.forEach(link => {
-    link.addEventListener('click', function () {
-      const currentActiveLink = reportDuration.querySelector('.active');
-      currentActiveLink.setAttribute('class', '');
-      this.setAttribute('class', 'active');
-    })
-  })
+  durationLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      currentActiveLink = reportDuration.querySelector(".active");
+      currentActiveLink.setAttribute("class", "");
+      this.setAttribute("class", "active");
+      main(this.textContent.toLowerCase());
+    });
+  });
 });
 
-function displayDetails() {
-  
+
+function main(selectedDuration) {
+  reportDetails.innerHTML = '';
+  const filteredData = getDisplayDetails(selectedDuration);
+  appendDetails(filteredData);
+}
+
+function getDisplayDetails(timeFrame) {
+  return reportData.map(({ title, timeframes }) => ({
+      title,
+      current: timeframes[timeFrame]["current"],
+      previous: timeframes[timeFrame]["previous"]
+    })
+  );
+}
+
+function appendDetails(data) {
+  data.forEach(({ title, current, previous }) => {
+    reportDetails.innerHTML += `
+      <article class="report-detail ${formatTitle(title)}">
+        <div>
+          <div class="report-title flex">
+            <h2>${title}</h2>
+            <img src="./images/icon-ellipsis.svg" alt="more report details" />
+          </div>
+          <div class="report-time flex">
+            <h3>${current}hrs</h3>
+            <p>Last Week  - ${previous}hrs</p>
+          </div>
+        </div>
+      </article>
+    `
+  })
+}
+
+function formatTitle(title) {
+  return title.replace(' ', '-').toLowerCase();
 }
